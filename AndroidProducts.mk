972b5d6638dfe854a6bbc0db3e6243087c082743
ArixElo <patroxgamer@gmail.com>
2023-06-28 17:56:23 +0000

    davinci: PixysOS bringup
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The tequilaOS
+# Copyright (C) 2023 The PixysOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/tequila_davinci.mk
+    $(LOCAL_DIR)/pixys_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    tequila_davinci-user \
-    tequila_davinci-userdebug \
-    tequila_davinci-eng
+    pixys_davinci-user \
+    pixys_davinci-userdebug \
+    pixys_davinci-eng
--- pixys_davinci.mk
+++ pixys_davinci.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The tequilaOS
+# Copyright (C) 2023 The PixysOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
-PRODUCT_NAME := tequila_davinci
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# PixysOS Properties
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+PRODUCT_NAME := pixys_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
